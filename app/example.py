bf41c45f0c46ae0f8cc1966afdd973373558ffcb
kmalakhova <kgrekhova@gmail.com>
2021-07-28 19:37:16 -0700

    Minor changes

--- app/example.py
+++ app/example.py
@@ -1,10 +1,10 @@
-from routes import search_word
+from word import get_dictionary_entry
 from word import get_random_word
 from extract import json_extract
 
 def get_word_example(word):
-    response_body = search_word(word)
-    word_examples = json_extract(response_body, 'text') # Find every instance of `text`
+    response_body = get_dictionary_entry(word)
+    word_examples = json_extract(response_body, 'text')
     for example in word_examples:
         if is_example_valid(example):
             return example
@@ -12,4 +12,5 @@ def get_word_example(word):
 
 def is_example_valid(example):
     return len(example.split()) < 10 and len(example.split()) > 5
+
 print(get_word_example(get_random_word()))
--- app/word.py
+++ app/word.py
@@ -1,15 +1,35 @@
-# from routes import search_word
 import random
-import requests
+from requests import get
+import os 
+from dotenv import load_dotenv
 
-def get_random_word(): # save loc
-    res = requests.get("https://raw.githubusercontent.com/dolph/dictionary/master/popular.txt")
-    # words = list(map(str, res.content.split()))
-    words = [word.decode() for word in res.content.split()]
-    word = random.choice(words)
-    # print(type(word))
-    # print(word)
-    return word
-    # return "cat"
+load_dotenv()
 
-# get_random_word()
+def get_random_word():
+    with open("./dictionary.txt", "r") as file:
+        allText = file.read()
+        words = list(map(str, allText.split()))
+        word = random.choice(words)
+        return word
+
+def is_in_dictionary(status_code):
+    return status_code == 200
+
+def get_dictionary_entry(word):
+    APP_KEY = os.environ.get("APP_KEY")
+    APP_ID = os.environ.get("APP_ID")
+    API_BASE_URL = "https://od-api.oxforddictionaries.com"
+    language = 'en-us'
+    fields = 'examples'
+    strictMatch = 'false'
+    url = API_BASE_URL + ':443/api/v2/entries/' + language + '/' + word + '?fields=' + fields + '&strictMatch=' + strictMatch;
+
+    response = get(url, headers = {'APP_ID': APP_ID, 'APP_KEY': APP_KEY})
+
+    status_code = response.status_code
+    if not is_in_dictionary(status_code):
+        word = get_random_word() 
+        # sleep
+        return get_dictionary_entry(word)
+    response_body = response.json()
+    return response_body
