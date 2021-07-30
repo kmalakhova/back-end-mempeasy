ca30cf586c273eb01a72090b75b9ecc0a2ec01cc
kmalakhova <kgrekhova@gmail.com>
2021-07-30 16:46:01 -0700

    Minor refactoring

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,9 @@ from flask import Blueprint
 from app.password import get_response
 from flask import json
 
-
 app_bp = Blueprint("app", __name__, url_prefix="/")
 
 @app_bp.route("", methods=["GET"], strict_slashes=False)
-def start_process():
+def start():
     password, hint = (get_response())
     return json.dumps({"password":password, "hint":hint}, indent = 4)
-
