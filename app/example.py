313006f8a09c5381297511e0ac64e0b23760747d
kmalakhova <kgrekhova@gmail.com>
2021-07-30 16:34:55 -0700

    Add lower_all_words()

--- app/example.py
+++ app/example.py
@@ -1,10 +1,13 @@
-from word import get_dictionary_entry
-from word import get_random_word
-from extract import json_extract
+from app.word import get_dictionary_entry, get_random_word
+from app.extract import json_extract
 
 def is_example_valid(example):
     """Validates that a sentence is of a suitable length"""
-    return len(example.split()) < 10 and len(example.split()) > 5
+    return len(example.split()) < 10 and len(example.split()) > 3
+
+def lower_all_words(example):
+    "Lowers all letters in a sentence"
+    return example.lower()
 
 def get_word_example(word):
     """Retrieves sentences with a given word from a response body"""
@@ -12,5 +15,6 @@ def get_word_example(word):
     word_examples = json_extract(response_body, 'text')
     for example in word_examples:
         if is_example_valid(example):
+            example = lower_all_words(example)
             return example
     return get_word_example(get_random_word())
