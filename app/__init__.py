5de5380cea9896b7188785b405053d980300f54a
kmalakhova <kgrekhova@gmail.com>
2021-07-30 16:02:01 -0700

    Register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ def create_app():
     app = Flask(__name__)
     app.config["APP_KEY"] = os.environ.get("APP_KEY")
     app.config["APP_ID"] = os.environ.get("APP_ID")
+
+    from app.routes import app_bp
+    app.register_blueprint(app_bp)
+
     return app
