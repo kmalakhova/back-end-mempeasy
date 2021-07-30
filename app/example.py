5efb9f8a37b6f3209a22e5abdce6fe19c6e04245
kmalakhova <kgrekhova@gmail.com>
2021-07-30 16:36:12 -0700

    Add lower_all_words()

--- app/example.py
+++ app/example.py
@@ -5,7 +5,7 @@ def is_example_valid(example):
     """Validates that a sentence is of a suitable length"""
     return len(example.split()) < 10 and len(example.split()) > 3
 
-def lower_all_words(example):
+def lower_all_letters(example):
     "Lowers all letters in a sentence"
     return example.lower()
 
@@ -15,6 +15,6 @@ def get_word_example(word):
     word_examples = json_extract(response_body, 'text')
     for example in word_examples:
         if is_example_valid(example):
-            example = lower_all_words(example)
+            example = lower_all_letters(example)
             return example
     return get_word_example(get_random_word())
