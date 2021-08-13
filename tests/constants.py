3a0c0c0b4fdf27904771ffa21a409790ab557514
kmalakhova <kgrekhova@gmail.com>
2021-08-12 20:13:20 -0700

    Minor changes in tests

---
+++ tests/constants.py
@@ -0,0 +1,6 @@
+TEST_STR = "the pig likes to dig"
+ONLY_LETTERS = ["the", "rat", "is", "fat"]
+WSYMBOL = ["can", "you", "be", "ready", "at", "five", "?"]
+WNUMBER = ["3",":","05","PM"]
+WSYMBOL_WNUMBER = ["it", "is", "Aug", "10", "already", "!"]
+SYMBOL = ["!","$",":",";","?","-"]
--- tests/test_password.py
+++ tests/test_password.py
@@ -1,46 +1,39 @@
 from app.password import *
-import pytest
-
-pytest.test_str = "the pig likes to dig"
-pytest.only_letters = ["the", "rat", "is", "fat"]
-pytest.wsymbol = ["can", "you", "be", "ready", "at", "five", "?"]
-pytest.wnumber = ["3",":","05","PM"]
-pytest.wsymbol_wnumber = ["it", "is", "Aug", "10", "already", "!"]
-pytest.symbols = ["!","$",":",";","?","-"]
+from tests.constants import *
 
 def test_uppercase_random_word():
-    sentence = uppercase_random_word(pytest.only_letters)
+    sentence = uppercase_random_word(ONLY_LETTERS)
     assert any(word == word.upper() for word in sentence)
 
 def test_sentence_has_symbol_returns_true():
-    assert has_symbol(pytest.wsymbol, pytest.symbols)
+    assert has_symbol(WSYMBOL, SYMBOL)
 
-def test_sentence_has_no_symbols_returns_false():
-    assert not has_symbol(pytest.only_letters, pytest.symbols)
+def test_sentence_has_no_SYMBOL_returns_false():
+    assert not has_symbol(ONLY_LETTERS, SYMBOL)
 
 def test_add_symbol_to_sentence():
-    sentence = uppercase_random_word(pytest.only_letters)
-    sentence = add_symbol(sentence, pytest.symbols)
-    assert has_symbol(sentence, pytest.symbols)
+    sentence = uppercase_random_word(ONLY_LETTERS)
+    sentence = add_symbol(sentence, SYMBOL)
+    assert has_symbol(sentence, SYMBOL)
 
 def test_sentence_has_number_returns_true():
-    assert has_number(pytest.wnumber)
+    assert has_number(WNUMBER)
 
 def test_sentence_has_no_numbers_returns_false():
-    assert not has_number(pytest.wsymbol)
+    assert not has_number(WSYMBOL)
 
 def test_add_random_number():
-    sentence = add_number(pytest.only_letters)
+    sentence = add_number(ONLY_LETTERS)
     assert has_number(sentence)    
 
 def test_get_first_letters():
-    assert get_first_letters([pytest.wsymbol_wnumber[0].upper()] \
-        + pytest.wsymbol_wnumber[1:]) == \
+    assert get_first_letters([WSYMBOL_WNUMBER[0].upper()] \
+        + WSYMBOL_WNUMBER[1:]) == \
             ["IT", 'i','A','10','a','!']
 
 def test_build_password():
-    password, hint = build_password(hint = pytest.test_str)
+    password, hint = build_password(hint = TEST_STR)
     split_password = list(password)
-    assert has_symbol(split_password, pytest.symbols)
+    assert has_symbol(split_password, SYMBOL)
     assert has_number(split_password)
     assert any(sym == sym.upper() for sym in split_password)
