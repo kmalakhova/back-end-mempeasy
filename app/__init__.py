88a30ccd8cb8cc884c0f09766b66116decf35389
kmalakhova <kgrekhova@gmail.com>
2021-08-12 20:14:14 -0700

    Add comment to ignore file in tests

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-def create_app():
+def create_app(): # pragma: no cover
     app = Flask(__name__)
     app.config["APP_KEY"] = os.environ.get("APP_KEY")
     app.config["APP_ID"] = os.environ.get("APP_ID")
